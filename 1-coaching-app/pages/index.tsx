91d538f54d9e92bd0d5e68ac267a918e12497329
Andy Fang <andy@boringordinary.com>
2020-12-02 13:25:29 -0500

    Improve layout

--- 1-coaching-app/pages/index.tsx
+++ 1-coaching-app/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   providers,
   SessionProvider,
@@ -8,13 +8,16 @@ import {
 } from "next-auth/client";
 import {
   Container,
-  Box,
   Button,
   TextField,
   LinearProgress,
   Grid,
+  Avatar,
+  Typography,
+  Snackbar,
 } from "@material-ui/core";
 import { FaDiscord } from "react-icons/fa";
+import { NextPageContext } from "next";
 
 interface Props {
   providers: SessionProvider;
@@ -22,61 +25,94 @@ interface Props {
 
 export default function Home({ providers }: Props) {
   const [session, loading] = useSession();
+  const [toastOpen, setToastOpen] = useState(false);
+  const [descriptionValue, setDescriptionValue] = useState("");
 
-  const AppForm = (
-    <>
+  if (loading) {
+    return <LinearProgress />;
+  }
+
+  const submitApp = () => {
+    setToastOpen(true);
+    return;
+  };
+
+  const AppForm = session ? (
+    <Grid container direction="column" spacing={2}>
+      <Grid item>
+        <Grid container alignItems="center" spacing={1}>
+          <Grid item>
+            <Avatar src={session.user.image}></Avatar>
+          </Grid>
+          <Grid item>
+            <Typography>{session.user.name}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
       <Grid item>
         <TextField
           label="Description"
           multiline
           variant="outlined"
           placeholder="What would you like your coach to know? E.g. feeling stuck in life"
+          value={descriptionValue}
+          onChange={(event) => setDescriptionValue(event.target.value)}
+          fullWidth
         ></TextField>
       </Grid>
       <Grid item>
-        <Button color="primary" variant="contained">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={submitApp}
+          disabled={descriptionValue === ""}
+          fullWidth
+          size="large"
+        >
           Register for coaching
         </Button>
       </Grid>
-    </>
-  );
+    </Grid>
+  ) : null;
 
-  if (loading) {
-    return <LinearProgress />;
-  }
-
+  console.log(providers);
   return (
-    <Container>
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          {!session &&
-            Object.values(providers).map((provider) => (
-              <div key={provider.name}>
-                <Button
-                  onClick={() => signIn(provider.id)}
-                  color="primary"
-                  variant="contained"
-                  startIcon={<FaDiscord />}
-                >
-                  Login with {provider.name}
-                </Button>
-              </div>
-            ))}
+    <>
+      <Container maxWidth="xs">
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            {!session && (
+              <Button
+                onClick={() => signIn("discord")}
+                color="primary"
+                variant="contained"
+                startIcon={<FaDiscord />}
+              >
+                Login with Discord
+              </Button>
+            )}
 
-          {session && (
-            <Button onClick={() => signOut()} size="large">
-              Logout
-            </Button>
-          )}
+            {session && (
+              <Button onClick={() => signOut()} size="large">
+                Logout
+              </Button>
+            )}
+          </Grid>
+
+          {AppForm}
         </Grid>
 
-        {session && AppForm}
-      </Grid>
-    </Container>
+        <Snackbar
+          open={toastOpen}
+          onClose={() => setToastOpen(false)}
+          message="Success! Your coach will reach out to you soon."
+        />
+      </Container>
+    </>
   );
 }
 
-Home.getInitialProps = async (context) => {
+Home.getInitialProps = async (context: NextPageContext) => {
   // providers is incorrectly typed in the library, so we're disabling it here.
   return {
     providers: await (providers as any)(context),
